7914d579385545e672124168aaf1e553f7a2950f
Francisco Gonzalez <fg@panch.io>
2021-05-04 21:06:02 -0500

    Add utils

---
+++ lfstitch/utils.py
@@ -0,0 +1,10 @@
+import os
+from typing import Optional
+import imutils
+
+def load_image(path: str, height:Optional[int]=None, width:Optional[int]=None):
+    assert os.path.exists(path)
+    img = cv2.imread(path)
+    if height or width:
+        img = imutils.resize(img, height=height, width=width)
+    return img
